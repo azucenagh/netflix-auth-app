ef7c44bc84742d1cc1520bd22f99e5b199bc8037
azucenagh <sweet.azu.app@gmail.com>
2025-07-17 19:36:42 -0600

    Modificacion de vista

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import RegisterView
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('register/', RegisterView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
 ]
